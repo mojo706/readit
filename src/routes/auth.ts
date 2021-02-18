72cd8a72bf2e5abd16f15512e18fc4098932e11a
basedHokage <chriseugene75@gmail.com>
2021-02-18 22:36:10 +0300

    add login route and some logic

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from "express";
 import { validate } from "class-validator"
+import bcrypt from "bcrypt"
 
 import { User } from "../entities/User"
 
@@ -7,7 +8,7 @@ const register = async (req: Request, res: Response ) => {
   const {email, username, password } = req.body
 
   try {
-    // TODO:Validate data
+    
     let errors: any = {}
     const emailUser = await User.findOne({email})
     const usernameUser = await User.findOne({username})
@@ -19,14 +20,14 @@ const register = async (req: Request, res: Response ) => {
       return res.status(400).json(errors)
     }
 
-    // TODO: Create the user
+    
     const user = new User({email, username, password})
 
     errors =  await validate(user)
     if(errors.length > 0) return res.status(400).json(errors)
 
     await user.save()
-    // TODO: Return the user
+    
     return res.json(user)
   } catch (err) {
     console.log(err)
@@ -34,6 +35,28 @@ const register = async (req: Request, res: Response ) => {
   }
 }
 
+const login = async (req: Request, res: Response) => {
+  const {username, password} = req.body
+
+  try {
+    const user = await User.findOne({username})
+    
+    if(!user) return res.status(400).json({error: 'User not found'})
+
+    const passwordMatches = await bcrypt.compare(password, user.password)
+
+    if(!passwordMatches) {
+      return res.status(401).json({password: 'Password is incorrect'})
+    }
+
+    return res.json(user)
+  } catch (err) {
+    
+  }
+}
+
 const router = Router()
 router.post('/register', register)
+router.post('/login', login)
+
 export default router
