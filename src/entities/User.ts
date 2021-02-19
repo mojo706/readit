f5bbb72701494ca5c2195082cde6232790d1080a
basedHokage <chriseugene75@gmail.com>
2021-02-19 21:29:06 +0300

    refactor code to make code cleaner

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,17 +1,15 @@
 import { IsEmail, Length } from "class-validator"
-import {Entity, PrimaryGeneratedColumn, Column, BaseEntity, Index, CreateDateColumn, UpdateDateColumn, BeforeInsert} from "typeorm"
+import {Entity as TOEntity, Column, Index, BeforeInsert} from "typeorm"
 import bcrypt from "bcrypt"
-import { classToPlain, Exclude } from "class-transformer";
-@Entity('users')
-export class User extends BaseEntity {
+import { Exclude } from "class-transformer";
+
+import Entity from "./Entity"
+@TOEntity('users')
+export default class User extends Entity {
     constructor(user: Partial<User>) {
         super()
         Object.assign(this, user)
     }
-
-    @Exclude()
-    @PrimaryGeneratedColumn()
-    id: number
 
     @Index()
     @IsEmail()
@@ -28,19 +26,8 @@ export class User extends BaseEntity {
     @Length(6, 255)
     password: string
 
-    @CreateDateColumn()
-    createdAt: Date
-
-    @UpdateDateColumn()
-    updatedAt: Date
-
     @BeforeInsert()
     async hashPassword(){
         this.password = await bcrypt.hash(this.password, 6)
     }
-
-    toJSON() {
-        return classToPlain(this)
-    }
-
 }
