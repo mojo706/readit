084ded3d0ab657dbd09c2477ca850e4d59b616ad
basedHokage <chriseugene75@gmail.com>
2021-02-19 15:22:47 +0300

    add some input validation :)

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from "express";
-import { validate } from "class-validator"
+import { isEmpty, validate } from "class-validator"
 import bcrypt from "bcrypt"
 
 import { User } from "../entities/User"
@@ -39,6 +39,13 @@ const login = async (req: Request, res: Response) => {
   const {username, password} = req.body
 
   try {
+    let errors: any = {}
+
+    if (isEmpty(username)) errors.username = 'Username must not be empty'
+    if (isEmpty(password)) errors.password = 'Password must not be empty'
+    if (Object.keys(errors).length > 0) {
+      return res.status(400).json(errors)
+    }
     const user = await User.findOne({username})
     
     if(!user) return res.status(400).json({error: 'User not found'})
